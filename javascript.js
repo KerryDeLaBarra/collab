5cfa2a8dca8a77808667af594f625781455b0155
Luciano Arancibia <luciano.arancibiav@gmail.com>
2025-01-21 00:25:26 -0300

    feat: Entrega final * ajax not used but XHR instead * use of native json * use of async and promises * use of array & events * use of external public api: https://dummyjson.com

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
 class Elemento {
-    constructor(nombre, precio) {
-        this.nombre = nombre;
-        this.precio = precio;
+    constructor(id, model, brand, color, price) {
+        this.id = id;
+        this.model = model;
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
     }
 }
 
 function eliminarElemento(nombreElemento) {
     let elementos = JSON.parse(localStorage.getItem('elementos'));
-    const elementoEncontrado = elementos.find(elemento => elemento.nombre === nombreElemento);
+    console.log(elementos, nombreElemento);
+    const elementoEncontrado = elementos.find(elemento => elemento.id === nombreElemento);
 
     if (elementoEncontrado) {
-        elementos = elementos.filter(elemento => elemento.nombre !== nombreElemento);
+        elementos = elementos.filter(elemento => elemento.id !== nombreElemento);
         localStorage.setItem('elementos',  JSON.stringify(elementos));
     } else {
         alert('EL ELEMENTO NO EXISTE');
@@ -23,50 +27,74 @@ function formatearNombre(nombre) {
     return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
 }
 
-function iniciar() {
-    let elementos = [
-        new Elemento(formatearNombre('Mástil de Rosewood'), 200),
-        new Elemento(formatearNombre('Cuerpo de Telecaster'), 300),
-        new Elemento(formatearNombre('Puente Evertune'), 500),
-    ];
-    localStorage.setItem('elementos',  JSON.stringify(elementos));
-    const formulario = document.getElementById("formulario");
-    const spanTotal = document.getElementById("total");
-
-    formulario.addEventListener("submit", crearProducto);
-    renderizarTotal();
-    renderTable();
-
+function sendHttpRequest(method, url, data) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = () => {
+      if (xhr.status >= 400) reject(xhr.response);
+      else resolve(xhr.response);
+    };
+    xhr.onerror = () => {
+      reject("Error en la llamada api!");
+    };
+    xhr.send(JSON.stringify(data));
+  });
 }
 
+function iniciar() {
+    sendHttpRequest("GET", "https://dummyjson.com/c/f972-101a-43ef-94dc")
+    .then((responseData) => {
+        const guitars = JSON.stringify(responseData.guitars);
+        localStorage.setItem('elementos',  guitars)
+    })
+    .then(() => {
+        const formulario = document.getElementById("formulario");
+        const spanTotal = document.getElementById("total");
+        formulario.addEventListener("submit", crearProducto);
+        renderizarTotal();
+        renderTable();
+    });
+}
 
 function nombreProductoExiste(nombre) {
     const elementos = JSON.parse(localStorage.getItem('elementos'));
     return elementos.some( (el) => {
-        return el.nombre.toLowerCase() === nombre.toLowerCase();
+        return el.model.toLowerCase() === nombre.toLowerCase();
     });
 }
 
 function crearProducto(e) {
     e.preventDefault();
 
-    const inputNombreProducto = document.getElementById("nombreProducto");
-    const inputPrecioProducto = document.getElementById("precioProducto");
+    const inputModel = document.getElementById("model");
+    const inputBrand = document.getElementById("brand");
+    const inputColor = document.getElementById("color");
+    const inputPrice = document.getElementById("price");
 
-    const nombreProducto = inputNombreProducto.value;
-    const precioProducto = parseFloat(inputPrecioProducto.value);
+    const modelName = inputModel.value;
+    const brandName = inputBrand.value;
+    const colorName = inputColor.value;
+    const price = parseFloat(inputPrice.value);
 
-    inputNombreProducto.value = "";
-    inputPrecioProducto.value = "";
+    inputModel.value = "";
+    inputBrand.value = "";
+    inputColor.value = "";
+    inputPrice.value = 0;
 
-    if(nombreProductoExiste(nombreProducto)) {
+    if(nombreProductoExiste(modelName)) {
         alert("PRODUCTO YA EXISTE");
         return;
     }
 
     const producto = new Elemento(
-       formatearNombre(nombreProducto),
-        precioProducto
+        Math.random(),
+        formatearNombre(modelName),
+        brandName,
+        colorName,
+        price
     );
 
     const elementos = JSON.parse(localStorage.getItem('elementos'));
@@ -77,8 +105,8 @@ function crearProducto(e) {
 }
 
 function renderizarTotal() {
-    const elementos = JSON.parse(localStorage.getItem('elementos'));
-    const total = elementos.reduce((suma, elemento) => suma + elemento.precio, 0);
+    const elementos = JSON.parse(localStorage.getItem('elementos'))
+    const total = elementos.reduce((suma, elemento) => suma + elemento.price, 0);
     localStorage.setItem('total',  total);
     const spanTotal = document.getElementById("total");
     spanTotal.innerText = total;
@@ -89,32 +117,37 @@ function renderTable() {
     var tbodyProductos = document.getElementById("tbodyProductos");
 
     tbodyProductos.innerHTML = "";
-    const elementos = JSON.parse(localStorage.getItem('elementos'));
+    const elementos = JSON.parse(localStorage.getItem('elementos'))
     for(const producto of elementos) {
 
         const tr = document.createElement("tr");
+        const tdModel = document.createElement("td");
+        const tdBrand = document.createElement("td");
+        const tdColor = document.createElement("td");
+        const tdPrice = document.createElement("td");
 
-        const tdNombre = document.createElement("td");
-        const tdPrecio = document.createElement("td");
         const tdAcciones = document.createElement("td");
 
-        tdNombre.innerText = `${producto.nombre}`;
+        tdModel.innerText = `${producto.model}`;
+        tdBrand.innerText = `${producto.brand}`;
+        tdColor.innerText = `${producto.color}`;
+
 
         const spanPrecio = document.createElement("span");
-        spanPrecio.innerText = `$${producto.precio}`;
+        spanPrecio.innerText = `$${producto.price}`;
 
-        tdPrecio.append(spanPrecio);
+        tdPrice.append(spanPrecio);
 
         const botonEliminar = document.createElement("button");
         botonEliminar.addEventListener('click', (e) => {
-            const nombreProducto = `${producto.nombre}`;
+            const nombreProducto = producto.id;
             eliminarElemento(nombreProducto);
 
         })
         botonEliminar.innerText = "Eliminar";
         tdAcciones.append(botonEliminar);
 
-        tr.append(tdNombre, tdPrecio, tdAcciones);
+        tr.append(tdModel, tdBrand, tdColor, tdPrice, tdAcciones);
         tbodyProductos.append(tr);
     }
 }
